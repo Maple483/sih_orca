985e621b20d88fc002079521fde6555c6cb5e6e2
Ewshita <176363365+Maple483@users.noreply.github.com>
2026-09-03 17:01:52 +0530

    Delete orca-frontend/productivity-home-back.js

--- orca-frontend/productivity-home-back.js
+++
@@ -1,9 +0,0 @@
-(() => {
-  document.addEventListener('click', (event) => {
-    const button = event.target.closest?.('#mp-close');
-    if (!button) return;
-    event.preventDefault();
-    event.stopImmediatePropagation();
-    window.location.reload();
-  }, true);
-})();
